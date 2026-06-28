83015cc7511bf2ef9ae11cb8af34154bac287d49
seoyeongma <tjdud3188@gmail.com>
2026-06-28 12:09:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+const menus=document.querySelectorAll(".menu");
+
+menus.forEach(menu=>{
+
+menu.addEventListener("mouseenter",()=>{
+
+menu.style.transform="scale(1.08)";
+
+});
+
+menu.addEventListener("mouseleave",()=>{
+
+menu.style.transform="scale(1)";
+
+});
+
+});
